cc6457e312ed89d2edb0bef93ca5446b93cd4eaa
eltonpan <pzhelton@gmail.com>
2024-03-03 18:17:48 -0500

    optimized CVAE beta (1e-2)

--- train_cvae.py
+++ train_cvae.py
@@ -16,9 +16,9 @@ from models.cvae import CVAEv1, CVAEv2
 configs = { 
             'model_type' : 'cvae',
             'split' : 'system',
-            'fname': 'v7',
-            'device' : 'cuda:2',
-            'beta' : 1.0, # 2.5e-5,
+            'fname': 'v15',
+            'device' : 'cuda:3',
+            'beta' : 1e-7, # 2.5e-5,
             'batch_size' : 2048,
             'n_epochs' : 5000,
             'lr' : 1e-4,
