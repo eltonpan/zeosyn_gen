379f30346068ecfc4e048f56751238ac1614924c
eltonpan <pzhelton@gmail.com>
2024-04-08 21:57:09 -0400

    clean up

--- train_gmm.py
+++ train_gmm.py
@@ -15,11 +15,6 @@ configs = {
             'model_type' : 'gmm',
             'split' : 'system',
             'fname': 'v0',
-            # 'device' : 'cuda:3',
-            # 'beta' : 1e-2, # optimal 1e-2,
-            # 'batch_size' : 2048,
-            # 'n_epochs' : 5000,
-            # 'lr' : 1e-4,
             'model_params':{
                         'n_centers': 50,
                         'syn_dims': 12, 
@@ -54,29 +49,6 @@ def train_gmm(model, configs):
     # Save model
     with open(f"runs/{configs['model_type']}/{configs['split']}/{configs['fname']}/model.pkl", "wb") as outfile:
         pickle.dump(model, outfile)
-    # pdb.set_trace()
-
-# """ simulate some data """
-# density_simulator = SkewNormal(random_seed=22)
-# X, Y = density_simulator.simulate(n_samples=3000)
-
-# """ fit density model """
-# model = LSConditionalDensityEstimation(ndim_x=1, ndim_y=1, n_centers=50,random_seed=22) # works
-# model.fit(X, Y)
-
-
-# """ query the conditional pdf and cdf """
-# x_cond = np.zeros((1, 1))
-# y_query = np.ones((1, 1)) * 0.1
-# prob = model.pdf(x_cond, y_query)
-# pdb.set_trace()
-
-# cum_prob = model.cdf(x_cond, y_query)
-
-# """ compute conditional moments & VaR  """
-# mean = model.mean_(x_cond)[0][0]
-# std = model.std_(x_cond)[0][0]
-# skewness = model.skewness(x_cond)[0]
 
 if __name__ == '__main__':
     model = LSConditionalDensityEstimation(ndim_x=configs['model_params']['zeo_feat_dims']+configs['model_params']['osda_feat_dims'], ndim_y=configs['model_params']['syn_dims'], n_centers=configs['model_params']['n_centers'],random_seed=0)
